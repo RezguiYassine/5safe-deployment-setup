bc8d9f69c1c42ab0af9bb2735096d55c24f1b1fa
Qubut <s-aahmed@haw-landshut.de>
2024-09-15 08:49:38 +0200

    dev(lang): add needed haskell pkgs

--- user/lang/haskell.nix
+++ user/lang/haskell.nix
@@ -1,8 +1,14 @@
-{ pkgs, ... }:
+{ pkgs, pkgs-haskell-ormolu, ... }:
 
 {
   home.packages = with pkgs; [
-      haskellPackages.haskell-language-server
-      haskellPackages.stack
-  ];
+    haskellPackages.haskell-language-server
+    haskellPackages.stack
+    haskellPackages.cabal-install
+    haskellPackages.ghc
+    haskellPackages.implicit-hie
+    # haskellPackages.ghcup
+  ]
+  # ++ [ pkgs-haskell-ormolu ];
+  ;
 }
