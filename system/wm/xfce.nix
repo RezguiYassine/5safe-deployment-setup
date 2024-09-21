91a6beac104a2ed28264f55a4fdfb8ee15547cdc
Qubut <s-aahmed@haw-landshut.de>
2024-09-21 19:42:20 +0200

    desktopEnv: add xfce4

---
+++ system/wm/xfce.nix
@@ -0,0 +1,11 @@
+{ config, pkgs, callPackage, ... }: {
+
+  services.xserver = {
+    enable = true;
+    desktopManager = {
+      xterm.enable = false;
+      xfce.enable = true;
+    };
+  };
+  # services.displayManager.defaultSession = "xfce";
+}
