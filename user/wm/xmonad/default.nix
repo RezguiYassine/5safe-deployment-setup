c6f71fc4c4eb35ac9e33c2e88417d8874a9352da
Qubut <s-aahmed@haw-landshut.de>
2024-09-23 13:37:06 +0200

    wm(xmonad): convert configuration to a haskell project

--- user/wm/xmonad/default.nix
+++ user/wm/xmonad/default.nix
@@ -1,14 +1,16 @@
-{inputs, ...}:
-
+{ pkgs, inputs, ... }:
+let 
+  xmonad-srid = ./xmonad-srid; 
+in
 {
+
   xsession.windowManager.xmonad = {
-   enable = true;
-   enableContribAndExtras = true;
-   ghcArgs = [
-      "-hidir /tmp" # place interface files in /tmp, otherwise ghc tries to write them to the nix store
-      "-odir /tmp" # place object files in /tmp, otherwise ghc tries to write them to the nix store
-      "-i${inputs.xmonad-contexts}" # tell ghc to search in the respective nix store path for the module
-    ];
-    config = builtins.readFile ./xmonad.hs;
- };
+    enable = true;
+    enableContribAndExtras = true;
+    extraPackages = haskellPackages: [
+    haskellPackages.xmonad-contrib
+    haskellPackages.containers
+  ];
+    config =  builtins.readFile "${xmonad-srid}/Main.hs";
+  };
 }
