22cf90862fd0be69dbd0ea378e0b0c298e4f2f8e
Qubut <s-aahmed@haw-landshut.de>
2024-09-24 13:10:05 +0200

    wm(picom): configured picom w/o conf file

--- user/wm/utils/picom/picom.nix
+++ user/wm/utils/picom/picom.nix
@@ -1,30 +1,94 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    picom
-  ];
+  services.picom = {
+    enable = true;
+    package = pkgs.picom-pijulius;
+    vSync = true;
+    backend = "glx";
+    wintypes = {
+      normal = { shadow = false;};
+      tooltip = { shadow = true; opacity = 0.75; focus = true; full-shadow = false; };
+      dock = { shadow = false; };
+      dnd = { shadow = false; };
+      popup_menu = { opacity = 1; };
+      dropdown_menu = { opacity = 1; };
+      }; 
+    shadow = false;
+    shadowOpacity = 0.75;
+    shadowOffsets = [
+      (-7)
+      (-7)
+    ];
+  
+    shadowExclude = [
+      "name = 'Notification'"
+      "class_g = 'Conky'"
+      "class_g ?= 'Notify-osd'"
+      "class_g = 'Cairo-clock'"
+      "class_g = 'slop'"
+      "class_g = 'Polybar'"
+      "_GTK_FRAME_EXTENTS@:c"
+    ];
 
-  nixpkgs.overlays = [
-    (self: super:
-      {
-        picom = super.picom.overrideAttrs (oldAttrs: rec {
-        version = "unstable-2021-10-23";
-        src = super.fetchFromGitHub {
-          owner = "pijulius";
-          repo = "picom";
-          rev = "982bb43e5d4116f1a37a0bde01c9bda0b88705b9";
-          sha256 = "sha256-YiuLScDV9UfgI1MiYRtjgRkJ0VuA1TExATA2nJSJMhM=";
-        };
+    opacityRules = [
+      "90:class_g    = 'xmobar'"
+      "90:class_g    = 'firefox'"
+      "90:class_g    = 'librewolf'"
+      "90:class_g    = 'gtkcord4'"
+      "90:class_g    = 'bottles'"
+      "90:class_g    = 'PrismLauncher'"
+      "90:class_g    = 'Navigator'"
+      "90:class_g    = 'Rofi'"
+      "90:class_g    = 'Geary'"
+      "90:class_g    = 'KeePassXC'"
+      "90:class_g    = 'gnome-calendar'"
+      "90:class_g    = 'NewsFlashGTK'"
+      "90:class_g    = 'Pavucontrol'"
+    ];
+    fade = false;
+    fadeSteps = [
+      0.01
+      0.01
+    ];
+    fadeDelta = 1;
+    fadeExclude = [];
+    settings = {
+    shadow-radius = 7;
+    no-fading-openclose = false;
 
-        meta = with builtins.lib; {
-          description = "A fork of picom featuring better animations";
-          homepage = "https://github.com/pijulius/picom";
-        };
-        });
-      }
-    )
-  ];
+    no-fading-destroyed-argb = false;
+    inactive-opacity = 1.0;
+    popup_menu = { opacity = 1.0; };
+    dropdown_menu = { opacity = 1.0; };
+    inactive-opacity-override = true;
+    active-opacity = 1.0;
+    focus-exclude = [
+      "class_g = 'Cairo-clock'"
+      "class_g = 'Bar'"                    # lemonbar
+      "class_g = 'slop'"                    # maim
+    ];
+    blur-background = false;
+    blur-background-exclude = [
+      "class_g = 'slop'"
+      "_GTK_FRAME_EXTENTS@:c"
+    ];
+    mark-wmwin-focused = true;
+    mark-ovredir-focused = true;
+    detect-rounded-corners = true;
+    detect-client-opacity = false;
+    refresh-rate = 0;
+    detect-transient = true;
+    detect-client-leader = true;
+    glx-no-stencil = true;
+    glx-no-rebind-pixmap = true;
+    use-damage = false;
+    log-level = "info";
+    };
 
-  home.file.".config/picom/picom.conf".source = ./picom.conf;
+  };
+  # home.packages = with pkgs; [
+  #   picom
+  # ];
+
 }
