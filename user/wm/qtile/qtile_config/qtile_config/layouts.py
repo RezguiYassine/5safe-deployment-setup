b4b4e62e25b15d0a4943c5555ccbba5d8c88b091
Qubut <s-aahmed@haw-landshut.de>
2024-10-17 14:45:05 +0200

    wm(qtile): add more floating rules

--- user/wm/qtile/qtile_config/qtile_config/layouts.py
+++ user/wm/qtile/qtile_config/qtile_config/layouts.py
@@ -1,27 +1,41 @@
 from libqtile import layout
-from libqtile.config import Drag, Group, Key, Match, Screen
+from libqtile.config import Drag, Group, Key, Match, Screen, ScratchPad
 
 layouts = [
-    layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
-    layout.Max(),
-    # Try more layouts by unleashing below layouts.
-    layout.Stack(num_stacks=2),
-    layout.Bsp(),
-    layout.Matrix(),
     layout.MonadTall(),
-    layout.MonadWide(),
-    layout.RatioTile(),
-    layout.Tile(),
     layout.TreeTab(),
-    layout.VerticalTile(),
+    layout.Max(),
     layout.Zoomy(),
+    layout.Stack(num_stacks=2),
 ]
 
 
 floating_layout = layout.Floating(
     float_rules=[
         # Run the utility of `xprop` to see the wm class and name of an X client.
+        Match(title="Application Finder"),
+        Match(title="Whisker Menu"),
+        Match(wm_instance_class="xfce4-popup-whiskermenu"),
+        Match(wm_instance_class="xfce4-terminal"),
+        Match(title="Myuzi"),
+        Match(title="scratchpad"),
+        Match(wm_class="gtkcord4"),
+        Match(wm_class="Geary"),
+        Match(wm_class="Pavucontrol"),
+        Match(wm_class="Syncthing GTK"),
+        Match(wm_class="Proton Mail Bridge"),
+        Match(wm_class="Zenity"),
         *layout.Floating.default_float_rules,
+        Match(wm_class="confirm"),
+        Match(wm_class="dialog"),
+        Match(wm_class="download"),
+        Match(wm_class="error"),
+        Match(wm_class="file_progress"),
+        Match(wm_class="notification"),
+        Match(wm_class="notify"),
+        Match(wm_class="popup_menu"),
+        Match(wm_class="splash"),
+        Match(wm_class="toolbar"),
         Match(wm_class="confirmreset"),  # gitk
         Match(wm_class="makebranch"),  # gitk
         Match(wm_class="maketag"),  # gitk
