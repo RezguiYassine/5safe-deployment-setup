dd5c64008ad5d793cea59c613977524aa52b757d
Qubut <s-aahmed@haw-landshut.de>
2024-10-17 14:45:59 +0200

    wm(qtile): add more floating hooks

--- user/wm/qtile/qtile_config/qtile_config/hooks.py
+++ user/wm/qtile/qtile_config/qtile_config/hooks.py
@@ -8,3 +8,29 @@ from libqtile import hook
 def autostart():
     home = os.path.expanduser("~/.config/qtile/autostart.sh")
     subprocess.call(home)
+
+
+@hook.subscribe.client_new
+def floating_dialogs(window):
+    dialog = window.window.get_wm_type() == "dialog"
+    transient = window.window.get_wm_transient_for()
+    if dialog or transient:
+        window.floating = True
+
+
+@hook.subscribe.client_new
+def idle_dialogues(window):
+    if (
+        (window.window.get_name() == "Search Dialog")
+        or (window.window.get_name() == "Module")
+        or (window.window.get_name() == "Goto")
+        or (window.window.get_name() == "IDLE Preferences")
+    ):
+        window.floating = True
+
+
+@hook.subscribe.client_new
+def blueman_float(window):
+    name: str = window.window.get_name()
+    if name.lower().find("blueman-manager") != -1:
+        window.floating = True
