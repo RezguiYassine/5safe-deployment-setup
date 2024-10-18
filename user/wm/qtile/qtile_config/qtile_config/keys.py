307ef833e4c06d7bcc5dfc133cca812323453d71
Qubut <s-aahmed@haw-landshut.de>
2024-10-18 07:47:29 +0200

    user(wm/qtile): add scratchpad

--- user/wm/qtile/qtile_config/qtile_config/keys.py
+++ user/wm/qtile/qtile_config/qtile_config/keys.py
@@ -1,17 +1,72 @@
 from libqtile.lazy import lazy
-from libqtile.config import Key, Group, Drag, Click
-from config import mod, terminal, shift, alt, control, backspace
-import utils as U
-from groups import workspaces
+from libqtile.config import Key, Group, Drag, Click, ScratchPad, DropDown
 from functools import reduce
 import operator
+
+mod = "mod4"
+terminal = "kitty"
+backspace = 14
+control = "control"
+shift = "shift"
+alt = "mod1"
+
+
+@lazy.function
+def switch_to_group_screen(qtile, groupname: str | None = None):
+    # This shouldn't happen...
+    if groupname is None:
+        return
+    group = qtile.groups_map[groupname]
+    # if group is already on a screen...
+    if group.screen:
+        qtile.focus_screen(group.screen.index)
+    # it's not on a screen
+    else:
+        group.cmd_toscreen()
+
+
+def switch_group(group: Group):
+    return Key(
+        [mod],
+        group.name,
+        switch_to_group_screen(groupname=group.name),
+        desc="Switch to group {}".format(group.name),
+    )
+
+    # return Key(
+    #     [mod],
+    #     group.name,
+    #     lazy.group[group.name].toscreen(),
+    #     desc=f"Switch to group {group.name}",
+    # )
+
+
+def move_window_and_switch_to_group(group: Group):
+    return Key(
+        [alt],
+        group.name,
+        lazy.window.togroup(group.name, switch_group=True),
+        desc=f"Switch to & move focused window to group {group.name}",
+    )
+
+
+def move_window_to_group(group: Group):
+    return Key(
+        [mod, shift],
+        group.name,
+        lazy.window.togroup(group.name),
+        desc="move focused window to group {}".format(group.name),
+    )
+
 
 switch_windows = [
     Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
     Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], backspace, lazy.layout.next(), desc="Move window focus to other window"),
+    Key(
+        [mod], "backspace", lazy.layout.next(), desc="Move window focus to other window"
+    ),
 ]
 
 shuffle_windows = [
@@ -34,18 +89,101 @@ grow_window = [
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
 ]
 
+workspaces = [Group(str(i)) for i in range(1, 10)]
 workspaces_keys = reduce(
     operator.add,
-    map(
-        lambda group: [U.switch_group(group), U.move_window_to_group(group)], workspaces
-    ),
+    map(lambda group: [switch_group(group), move_window_to_group(group)], workspaces),
 )
+
+scratchpads = [
+    ScratchPad(
+        "scratchpad",
+        [
+            DropDown(
+                "term",
+                "alacritty --class scratch",
+                width=0.8,
+                height=0.8,
+                x=0.1,
+                y=0.1,
+                opacity=0.9,
+            ),
+            DropDown(
+                "ranger",
+                "alacritty --class=ranger -e ranger",
+                width=0.8,
+                height=0.8,
+                x=0.1,
+                y=0.1,
+                opacity=0.9,
+            ),
+            DropDown(
+                "volume",
+                "pavucontrol",
+                width=0.8,
+                height=0.8,
+                x=0.1,
+                y=0.1,
+                opacity=0.9,
+            ),
+            DropDown(
+                "thunar", "thunar", width=0.8, height=0.8, x=0.1, y=0.1, opacity=1
+            ),
+        ],
+    )
+]
+groups = workspaces + scratchpads
+
+scratchpads_keys = [
+    Key(
+        [mod],
+        "g",
+        lazy.group["scratchpad"].dropdown_toggle("term"),
+        desc="Launch terminal",
+    ),
+    Key(
+        [mod],
+        "r",
+        lazy.group["scratchpad"].dropdown_toggle("ranger"),
+        desc="Launch ranger",
+    ),
+    Key(
+        [mod],
+        "b",
+        lazy.group["scratchpad"].dropdown_toggle("volume"),
+        desc="Launch pavucontrol",
+    ),
+    Key(
+        [mod],
+        "e",
+        lazy.group["scratchpad"].dropdown_toggle("thunar"),
+        desc="Launch thunar",
+    ),
+]
+
 
 launch_programs = [
     Key([mod], "t", lazy.spawn(terminal), desc="Launch terminal"),
+    Key(
+        [mod, shift],
+        "t",
+        lazy.spawn("xfce4-terminal --drop-down"),
+        desc="Launch terminal",
+    ),
     Key([mod], "grave", lazy.spawn("firefox"), desc="Launch firefox"),
     Key([mod], "o", lazy.spawn("obsidian"), desc="Launch obsidian"),
-    Key([mod], 39, lazy.spawn("xfce4-appfinder"), desc="Launch xfce's app-finder"),
+    Key(
+        [mod],
+        "semicolon",
+        lazy.spawn("xfce4-appfinder"),
+        desc="Launch xfce's app-finder",
+    ),
+    Key(
+        [mod],
+        "escape",
+        lazy.spawn("xfce4-session-logout"),
+        desc="Launch logout options",
+    ),
 ]
 
 layouts_control = [
@@ -56,6 +194,7 @@ layouts_control = [
         desc="Toggle between split and unsplit sides of stack",
     ),
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+    Key([mod, shift], "Tab", lazy.prev_layout(), desc="Toggle between layouts"),
     Key(
         [mod],
         "f",
@@ -68,14 +207,42 @@ layouts_control = [
         lazy.window.toggle_floating(),
         desc="Toggle floating on the focused window",
     ),
+    Key([mod], "m", lazy.layout.maximize(), desc="use max layout"),
+    Key(
+        [mod],
+        "equal",
+        lazy.layout.grow_left().when(layout=["bsp", "columns"]),
+        lazy.layout.grow().when(layout=["monadtall", "monadwide"]),
+        desc="Grow window to the left",
+    ),
+    Key(
+        [mod],
+        "minus",
+        lazy.layout.grow_right().when(layout=["bsp", "columns"]),
+        lazy.layout.shrink().when(layout=["monadtall", "monadwide"]),
+        desc="Grow window to the right",
+    ),
 ]
 
 
 control_keys = [
-    Key([mod, shift], "q", lazy.window.kill(), desc="Kill focused window"),
+    Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
     Key([mod, control], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, control], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+    Key([mod], "x", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+    Key(
+        [],
+        "XF86MonBrightnessUp",
+        lazy.spawn("brightnessctl set +10"),
+    ),
+    Key(
+        [],
+        "XF86MonBrightnessDown",
+        lazy.spawn("brightnessctl set 10-"),
+    ),
+    Key([], "xF86AudioLowerVolume", lazy.spawn("pamixer -d 10")),
+    Key([], "XF86AudioRaiseVolume", lazy.spawn("pamixer -i 10")),
+    Key([], "XF86AudioMute", lazy.spawn("pamixer -t")),
 ]
 # Add key bindings to switch VTs in Wayland.
 # We can't check qtile.core.name in default config as it is loaded before qtile is started
@@ -95,6 +262,7 @@ keys = [
     *shuffle_windows,
     *grow_window,
     *workspaces_keys,
+    *scratchpads_keys,
     *launch_programs,
     *layouts_control,
     *control_keys,
