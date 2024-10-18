ff037c88e8066eecc966db9a0049169b23524c97
Qubut <s-aahmed@haw-landshut.de>
2024-10-18 06:59:41 +0200

    wm(xfce): add some plugins and config options

--- system/wm/xfce.nix
+++ system/wm/xfce.nix
@@ -1,10 +1,22 @@
 { config, pkgs, callPackage, ... }: {
+  environment.systemPackages =  (with pkgs.xfce; [
+    xfce4-pulseaudio-plugin
+    xfce4-xkb-plugin
+    thunar-archive-plugin
+    xfce4-whiskermenu-plugin
 
+  ])
+  # Broken! 
+  # ++ (with pkgs; [xmonad_log_applet_xfce])
+  ;
   services.xserver = {
     enable = true;
     desktopManager = {
       xterm.enable = false;
-      xfce.enable = true;
+      xfce = {
+        enable = true;
+        enableScreensaver = true;
+      };
     };
   };
   # services.displayManager.defaultSession = "xfce";
