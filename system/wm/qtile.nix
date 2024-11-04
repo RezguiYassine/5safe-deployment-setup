e16f9f49d260c91e502b4463802778fb52abe345
Qubut <s-aahmed@haw-landshut.de>
2024-11-05 00:19:56 +0100

    system(wm/qtile): disable wayland qtile

--- system/wm/qtile.nix
+++ system/wm/qtile.nix
@@ -1,27 +1,43 @@
-{ config, pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 {
-  nixpkgs.overlays = [
-  (self: super: {
-    qtile-unwrapped = super.qtile-unwrapped.overrideAttrs(_: rec {
-      postInstall = let
-        qtileSession = ''
-        [Desktop Entry]
-        Name=Qtile Wayland
-        Comment=Qtile on Wayland
-        Exec=qtile start -b wayland
-        Type=Application
-        '';
-        in
-        ''
-      mkdir -p $out/share/wayland-sessions
-      echo "${qtileSession}" > $out/share/wayland-sessions/qtile.desktop
-      '';
-      passthru.providedSessions = [ "qtile" ];
-    });
-  })
-];
+  # nixpkgs.overlays = [
+  #   (self: super: {
+  #     qtile-unwrapped = super.qtile-unwrapped.overrideAttrs (_: rec {
+  #       postInstall =
+  #         let
+  #           qtileSession = ''
+  #             [Desktop Entry]
+  #             Name=Qtile Wayland
+  #             Comment=Qtile on Wayland
+  #             Exec=qtile start -b wayland
+  #             Type=Application
+  #           '';
+  #         in
+  #         ''
+  #           mkdir -p $out/share/wayland-sessions
+  #           echo "${qtileSession}" > $out/share/wayland-sessions/qtile.desktop
+  #         '';
+  #       passthru.providedSessions = [ "qtile" ];
+  #     });
+  #   })
+  # ];
 
-services.displayManager.sessionPackages = with pkgs; [ qtile-unwrapped ];
-services.xserver.windowManager.qtile.enable = true;
+  # services.displayManager.sessionPackages = with pkgs; [ qtile-unwrapped ];
+  services.xserver.windowManager.qtile = {
+    enable = true;
+    extraPackages =
+      python3Packages: with pkgs; [
+        python312Packages.qtile
+        python312Packages.qtile-extras
+        python312Packages.pywlroots
+        python312Packages.pywayland
+        python312Packages.xkbcommon
+      ];
+  };
 }
