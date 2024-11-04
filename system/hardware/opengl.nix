28d2f87909b0e8ec0ad70cd6236db9366003e851
Qubut <s-aahmed@haw-landshut.de>
2024-11-05 00:14:10 +0100

    hardware: add intel media drivers

--- system/hardware/opengl.nix
+++ system/hardware/opengl.nix
@@ -3,8 +3,11 @@
 {
   # OpenGL
   hardware.opengl.enable = true;
+  hardware.opengl.driSupport = true;
   hardware.opengl.driSupport32Bit = true;
   hardware.opengl.extraPackages = with pkgs; [
     rocmPackages.clr.icd
+    intel-media-driver
   ];
+  environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; };
 }
