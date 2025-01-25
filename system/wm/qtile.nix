f3518ce0116316a254d3a6b60d827aecd1154756
Qubut <s-aahmed@haw-landshut.de>
2025-01-25 19:09:18 +0100

    sys(wm): define qtile extra Pkgs

--- system/wm/qtile.nix
+++ system/wm/qtile.nix
@@ -9,11 +9,10 @@
   services.xserver.windowManager.qtile = {
     enable = true;
     extraPackages =
-      python3Packages: with pkgs; [
-        python312Packages.qtile
-        python312Packages.pywlroots
-        python312Packages.pywayland
-        python312Packages.xkbcommon
+      python3Packages:  with python3Packages; [
+        pywlroots
+        pywayland
+        xkbcommon
       ];
   };
 }
