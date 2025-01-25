ae8de51096452ba8d8ebccb0338afbe41685e1b8
Qubut <s-aahmed@haw-landshut.de>
2025-01-25 19:08:45 +0100

    sys(harware/opengl): remove unnecessary variables

--- system/hardware/opengl.nix
+++ system/hardware/opengl.nix
@@ -1,12 +1,10 @@
 { pkgs, ... }:
 
 {
-  # OpenGL
   hardware.graphics.enable = true;
   hardware.graphics.enable32Bit = true;
   hardware.graphics.extraPackages = with pkgs; [
     rocmPackages.clr.icd
     intel-media-driver
   ];
-  environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; };
 }
