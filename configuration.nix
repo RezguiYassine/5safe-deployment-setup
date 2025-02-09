228de776a92b48cb4861e913a2af525a747285ad
Qubut <s-aahmed@haw-landshut.de>
2025-02-09 19:58:25 +0100

    remove unnecessary comments and add security module

--- configuration.nix
+++ configuration.nix
@@ -23,6 +23,7 @@
     ./system/hardware/printing.nix
     ./system/hardware/systemd.nix
     ./system/hardware/time.nix
+    ./system/security
     ./system/services.nix
     (import ./system/app/docker.nix {
       storageDriver = null;
@@ -152,20 +153,7 @@
     "flakes"
   ];
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   programs.xfconf.enable = true;
   programs.thunar.enable = true;
   programs.nm-applet.enable = true;
@@ -173,6 +161,13 @@
     thunar-archive-plugin
     thunar-volman
   ];
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
 
 }
