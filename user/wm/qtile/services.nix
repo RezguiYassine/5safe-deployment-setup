2bb778e364610cf9da4676b90f30d0eaf0989d6e
Qubut <s-aahmed@haw-landshut.de>
2025-02-09 19:38:09 +0100

    user(wm/qtile): correct dunst color definitions and add transparency

--- user/wm/qtile/services.nix
+++ user/wm/qtile/services.nix
@@ -1,13 +1,13 @@
-{ config, ... }: {
+{ config, pkgs, ... }: {
   services.dunst = {
     enable = true;
     settings = {
       global = {
-        follow = "mouse";
         width = 300;
         height = 300;
         origin = "bottom-right";
         offset = "10x50";
+        transparency = 10;
         progress_bar = true;
         corner_radius = 10;
         frame_width = 2;
@@ -15,33 +15,32 @@
         horizontal_padding = 8;
         font = "Droid Sans 9";
         separator_color = "frame";
-        frame_color = config.lib.stylix.colors.base0D;
+        frame_color = "#${config.lib.stylix.colors.base0D}";
         alignment = "left";
         vertical_alignment = "center";
         line_height = 0;
         markup = "full";
-        format = "<b>%s</b>\n%b";
         sort = true;
         idle_threshold = 120;
         browser = "${pkgs.xdg-utils}/bin/xdg-open";
       };
 
       urgency_low = {
-        background = config.lib.stylix.colors.base00;
-        foreground = config.lib.stylix.colors.base05;
+        background = "#${config.lib.stylix.colors.base00}";
+        foreground = "#${config.lib.stylix.colors.base05}";
         timeout = 5;
       };
 
       urgency_normal = {
-        background = config.lib.stylix.colors.base00;
-        foreground = config.lib.stylix.colors.base05;
+        background = "#${config.lib.stylix.colors.base00}";
+        foreground = "#${config.lib.stylix.colors.base07}";
         timeout = 10;
       };
 
       urgency_critical = {
-        background = config.lib.stylix.colors.base00;
-        foreground = config.lib.stylix.colors.base08;
-        frame_color = config.lib.stylix.colors.base08;
+        background = "#${config.lib.stylix.colors.base00}";
+        foreground = "#${config.lib.stylix.colors.base08}";
+        frame_color = "#${config.lib.stylix.colors.base08}";
         timeout = 0;
       };
     };
