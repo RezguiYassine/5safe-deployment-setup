71f919b23ed31de03a4d14ee2cb83e1c1f6ffa78
Qubut <s-aahmed@haw-landshut.de>
2025-02-09 19:12:31 +0100

    sys(wm/x11): adjust imports and display-managers

--- system/wm/x11.nix
+++ system/wm/x11.nix
@@ -1,11 +1,12 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
-  imports = [ ./pipewire.nix
-              ./dbus.nix
-              ./gnome-keyring.nix
-              ./fonts.nix
-            ];
+  imports = [
+    ./pipewire.nix
+    ./dbus.nix
+    ./gnome-keyring.nix
+    ./fonts.nix
+  ];
 
   # Configure X11
   services.xserver = {
@@ -20,17 +21,31 @@
       lightdm = {
         enable = true;
       };
+      gdm = {
+        enable = false;
+        wayland = true;
+      };
+
       sessionCommands = ''
-      xset -dpms
-      xset s blank
-      xset s 300
-      xset r rate 350 100
-      ${pkgs.lightlocker}/bin/light-locker --idle-hint &
-    '';
+        xset -dpms
+        xset s blank
+        xset s 300
+        xset r rate 350 100
+        ${pkgs.lightlocker}/bin/light-locker --idle-hint &
+      '';
     };
 
   };
   services.libinput = {
-      touchpad.disableWhileTyping = true;
+    touchpad.disableWhileTyping = true;
+  };
+  services.displayManager = {
+    sddm = {
+      enable = false;
+      wayland.enable = true;
+      enableHidpi = true;
+      theme = "chili";
+      package = lib.mkForce pkgs.kdePackages.sddm;
     };
+  };
 }
