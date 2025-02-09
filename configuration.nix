07350ee2cda983c20c216e84d9c05b9eab1d9e4a
Qubut <s-aahmed@haw-landshut.de>
2025-02-09 20:11:54 +0100

    remove unnecessary comments

--- configuration.nix
+++ configuration.nix
@@ -119,17 +119,6 @@
     timeshift
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # XDG_BASE DIRs
   environment.sessionVariables = {
     XDG_CACHE_HOME = "$HOME/.cache";
     XDG_CONFIG_HOME = "$HOME/.config";
