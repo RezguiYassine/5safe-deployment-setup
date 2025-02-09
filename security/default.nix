2706cd51cb5d912f507550d911fe1306ccb49f03
Qubut <s-aahmed@haw-landshut.de>
2025-02-09 19:57:02 +0100

    create security module

---
+++ security/default.nix
@@ -0,0 +1,13 @@
+{...}:
+{
+  imports = [
+    ./automount.nix
+    ./blocklist.nix
+    ./doas.nix
+    ./firejail.nix
+    ./firewall.nix
+    ./gpg.nix
+    ./openvpn.nix
+    ./sshd.nix
+  ];
+}
