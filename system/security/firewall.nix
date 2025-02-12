462a5d1d4f1dff5c117468bbe44bcd57820c4fea
Qubut <s-aahmed@haw-landshut.de>
2025-02-12 15:00:41 +0000

    close all ports

--- system/security/firewall.nix
+++ system/security/firewall.nix
@@ -2,6 +2,6 @@
 
 {
   networking.firewall.enable = true;
-  networking.firewall.allowedTCPPorts = [ 22000 21027 ]; # syncthing
-  networking.firewall.allowedUDPPorts = [ 22000 21027 ]; # syncthing
+  networking.firewall.allowedTCPPorts = []; 
+  networking.firewall.allowedUDPPorts = [];
 }
