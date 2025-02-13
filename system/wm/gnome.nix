028fcb2286208e3dfee9931a52d881809739379e
Qubut <s-aahmed@haw-landshut.de>
2025-02-13 11:26:43 +0100

    sys(wm): add gnome config

---
+++ system/wm/gnome.nix
@@ -0,0 +1,21 @@
+{pkgs, ...}:
+{
+    environment.gnome.excludePackages = (with pkgs; [
+        atomix # puzzle game
+        cheese # webcam tool
+        epiphany # web browser
+        evince # document viewer
+        geary # email reader
+        gedit # text editor
+        gnome-characters
+        gnome-music
+        gnome-photos
+        gnome-terminal
+        gnome-tour
+        hitori # sudoku game
+        iagno # go game
+        tali # poker game
+        totem # video player
+        ]);
+    services.xserver.desktopManager.gnome.enable = true;
+}
