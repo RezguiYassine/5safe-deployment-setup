c522e382e62a13c698271c85a6943e23cad2913f
Qubut <s-aahmed@haw-landshut.de>
2025-03-19 14:01:00 +0100

    correct pathes

--- system/hardware/default.nix
+++ system/hardware/default.nix
@@ -1,12 +1,12 @@
 {...}:
 {
   imports = [
-   ./system/hardware/opengl.nix
-   ./system/hardware/bluetooth.nix
-   ./system/hardware/power.nix
-   ./system/hardware/printing.nix
-   ./system/hardware/systemd.nix
-   ./system/hardware/time.nix
+   ./opengl.nix
+   ./bluetooth.nix
+   ./power.nix
+   ./printing.nix
+   ./systemd.nix
+   ./time.nix
   ];
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
